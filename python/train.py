aad571afe9725c500d1d4cef48ac9c35b21542da
arthurdoelp <arthurdoelp@gmail.com>
2021-07-21 11:24:03 -0400

    changed to try to run the code from through the app

--- python/train.py
+++ python/train.py
@@ -70,14 +70,14 @@ kpredictions = kmodel.predict(pred_images)
 
 
 model_predictions_df = pd.DataFrame({'kpredictions': list(kpredictions), 'ids': list(ids)}, columns=['kpredictions', 'ids'])
-print(model_predictions_df.head())
+# print(model_predictions_df.head())
 
 
 # SONG PREDICTION
 
-# filename = sys.argv[1]
+filename = sys.argv[1]
 # filename = '/Users/arthurdoelp/dev/projects/python-projects/music-vision/uploads/03_Baby_Cant_Leave_it_Alone.m4a'
-filename = '/app/uploads/03_Baby_Cant_Leave_it_Alone.m4a'
+# filename = '/app/uploads/03_Baby_Cant_Leave_it_Alone.m4a'
 filepath = os.path.abspath(filename)
 # print(str(filepath))
 file_id = filename[13:-4]
@@ -96,12 +96,12 @@ plot.pcolormesh(times, freqs, amplitudes)
 plot.axis('off')
 plot.subplots_adjust(left=0,right=1,bottom=0,top=1)
 # Save the file to the prediction_image folder with the file name
-prediction_image_file_path = os.path.abspath("/app/prediction_image") + "/" + file_id + '.png'
+prediction_image_file_path = os.path.abspath("prediction_image") + "/" + file_id + '.png'
 plot.savefig(prediction_image_file_path)
 
 
 # Run the prediction method to compare prediction image against the model
-pred_image_dir = os.path.abspath("/app/prediction_image")
+pred_image_dir = os.path.abspath("prediction_image")
 pred_image_glob_dir = pred_image_dir + '/*.png'
 img = [cv2.resize(cv2.imread(file), (224, 224)) for file in glob.glob(pred_image_glob_dir)]
 img = np.array(np.float32(img).reshape(len(img), -1)/255)
@@ -110,7 +110,7 @@ img = np.array(np.float32(img).reshape(len(img), -1)/255)
 kprediction = kmodel.predict(img)
 kprediction = str(kprediction).strip('[]')
 kprediction = int(kprediction)
-print("Cluster:",kprediction)
+# print("Cluster:",kprediction)
 
 
 # Collect Song Ids
@@ -119,7 +119,7 @@ filtered_model_predictions_df = model_predictions_df[model_predictions_df["kpred
 song_ids = ["Date"] + filtered_model_predictions_df
 
 # Load Song Performance Dataset
-song_performance_excel_filepath = os.path.abspath("/app/songs_dataset_sample.csv")
+song_performance_excel_filepath = os.path.abspath("songs_dataset_sample.csv")
 songs_df = pd.read_csv(song_performance_excel_filepath)
 
 # Filter dataset to reflect only the most similar songs
